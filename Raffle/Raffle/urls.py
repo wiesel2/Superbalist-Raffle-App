3957f75bb320993c80993798f0c4283be270e274
adamleozar <adamzar100@gmail.com>
2016-11-23 14:13:58 +0200

    Add files via upload
    Added A view for the home page. Add a form to take in names from user. Added the form to the view. Added a list to store names on local PC.

--- Raffle/Raffle/urls.py
+++ Raffle/Raffle/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'Raffle.views.home', name='home'),
+    url(r'^$', 'Raffle_App.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- Raffle/Raffle_App/views.py
+++ Raffle/Raffle_App/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import homeForm
 
-# Create your views here.
+names_list = []
+def home(request):
+
+    title = "Welcome to the raffle"
+    description = "Enter names into the raffle and then click draw to view the winner!"
+    form = homeForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "description": description,
+        "form": form,
+
+    }
+    if form.is_valid():
+        instance = form.cleaned_data.get("full_name")
+        names_list.append(instance)
+
+    print names_list
+
+
+
+    return render(request, "home.html", context)
